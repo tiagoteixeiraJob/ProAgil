0bac9312be69c93208cdd5726a97854f3e6f8cab
tiago <tiagoteixeirajob@gmail.com>
2020-02-14 18:50:16 -0300

    Angular project included SPA

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -29,7 +29,10 @@ namespace ProAgil.WebAPI
         {
             services.AddControllers();
 
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(
+                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+            );
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +42,8 @@ namespace ProAgil.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
